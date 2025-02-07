7b2733ff6c08d51061433c022695c81484ae57a2
NotJayy11 <elijah.deluna1102@gmail.com>
2025-02-07 21:18:57 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+console.log("Hello World!")
+console.log("Elijah De Luna")
+
+
+let variableName = "Elijah De Lunaaaaa"
+console.log(variableName)
+
+for (let x = 1; x <= 100; x += 2) {
+    console.log(x);}
